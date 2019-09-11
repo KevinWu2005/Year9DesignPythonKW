c758990edda5b6af71084fc1d45bc39c79f0edaa
Kevin Wu <Kevin.Wu@GCCYJPYRJ1WK.local>
2019-09-11 10:46:10 -0400

    taking input

---
+++ TakingInputs.py
@@ -0,0 +1,24 @@
+#Comments are made with a #
+#It is essential you comment code
+
+#This program will take two integers 
+#and multiply them
+
+#Input
+#The input function returns the string the user entered
+#All inputs start as Strings
+#To change the type of variable you "cast" it
+#Casting is the process of changing type
+name = input("Please input your name: ")
+a = input("Please input first number: ")
+a = int(a)
+b = input("Please input second number: ")
+b = int(b)
+#Process
+#What is Process
+product = a * b
+
+#Output
+print("Hi, "+name)
+print("The product of "+str(a)+" and "+str(b)+" is "+str(product)+".")
+
