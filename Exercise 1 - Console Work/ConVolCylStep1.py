9a180aeac6a4d1dddc6e3d43fdfe181a98da1d77
KevinWu2005 <54950545+KevinWu2005@users.noreply.github.com>
2019-09-17 11:37:41 -0400

    Add files via upload

---
+++ Exercise 1 - Console Work/ConVolCylStep1.py
@@ -0,0 +1,18 @@
+#Input
+#What inputs are needed to calculate the volume of a cylinder?
+name = input("What is your name: ")
+
+radius = input("Input radius (cm): ")
+radius = int(radius)
+
+height = input("Input height (cm): ")
+height = int(height)
+
+#Process
+#What formula is used to calculate the volume of a cylinder?
+volume = 3.14 * radius * radius * height
+
+#Output
+#What is important about the output?
+print("The volume is: ",volume)
+
