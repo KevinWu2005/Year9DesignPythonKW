af8289e29a2d44bb6998af1d6775626bda855c69
Kevin Wu <Kevin.Wu@GCCYJPYRJ1WK.local>
2019-10-03 10:31:20 -0400

    BasicGUI

--- BasicGUI.py
+++ BasicGUI.py
@@ -2,19 +2,27 @@ import tkinter as tk
 
 print("Start Program")
 root = tk.Tk() #builds your main window
-
+root.title("Story Writing Practice")
 #Widget/Element is an element in a GUI, Button, Textbox, input box, slider
 
 #Step 1: Construct the widget.
 btn1 = tk.Button(root)
 #Step 2: Configure the widget.
-btn1.config(text = "I am a button", width = 100, height = 3)
+btn1.config(text = "Type Below", width = 100, height = 3)
 #Step 3: Place the widget - pack(), grid(), 
 btn1.pack()
 
 output = tk.Text(root, width = 100, height = 20)
 output.config()
 output.pack()
+
+#new widget
+#Step 1: Construct the widget.
+btn1 = tk.Button(root)
+#Step 2: Configure the widget.
+btn1.config(text = "Every secret of a writer's soul, every experience of his life, every quality of his mind, is written large in his works", width = 100, height = 3)
+#Step 3: Place the widget - pack(), grid(), 
+btn1.pack()
 
 root.mainloop()
 
