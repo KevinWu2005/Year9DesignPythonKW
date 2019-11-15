acf6623f18ab8c9351caa3c9b6911893a46d3107
Kevin Wu <Kevin.Wu@GCCYJPYRJ1WK.local>
2019-11-15 10:12:53 -0500

    GUIAlmostDone

--- 3DShapesCalculator/3DShapesCalculator1.py
+++ 3DShapesCalculator/3DShapesCalculator1.py
@@ -6,8 +6,26 @@ root = tk.Tk()
 root.title("3DShapesCalculator")
 # configuring size of the window 
 root.geometry('750x500')
-#Tab Name Labels
-ttk.Label(text="3DShapesCalculator").grid(column=0, row=0, padx=310, pady=50)
+
+title = tk.Label(root,text="3DShapesCalculator!")
+title = tk.Label(root, text="3DShapesCalculator!", font=("3DShapesCalculator", 32))
+title.pack()
+
+#Cone Button
+Cone = tk.Button(root)
+Cone.config(text = "Cone Volume Calculator", width = 10, height = 10)
+Cone.pack()
+
+#Cylinder Button
+Cylinder = tk.Button(root)
+Cylinder.config(text = "Cylinder Volume Calculator", width = 10, height = 10)
+Cylinder.pack()
+
+#Cube Button
+Cube = tk.Button(root)
+Cube.config(text = "Cube Volume Calculator", width = 10, height = 10)
+Cube.pack()
+
 
 #Calling Main()
 root.mainloop()
